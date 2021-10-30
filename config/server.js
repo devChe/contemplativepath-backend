84980a47d4b766afa87d2c8f97864d18d92e7d30
devChe <eugeniostevenson26@gmail.com>
2021-10-31 02:25:49 +0800

    First commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '130feee6af958e90561743f365d20b49'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a974ad8b-23c0-4bea-8e8f-8664af20ca5d'
+};
